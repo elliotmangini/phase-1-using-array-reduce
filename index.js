1df8a99111d169ad7f69db41f79fd4a10d7e93b3
Elliot Mangini <elliot.mangini@gmail.com>
2022-08-12 09:31:00 -0600

    commit tree 7ec972a5929860c131fdd3861cb65634be72cb07

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+// inline function declaration
+
+// let totalBatteries = batteryBatches.reduce(function sumArray(previousValue, currentValue) {
+//     return previousValue + currentValue;
+// });
+
+
+// isolated function declaration
+
+// let totalBatteries = batteryBatches.reduce(sumArray);
+
+// function sumArray(previousValue, currentValue) {
+//     return previousValue + currentValue;
+// };
+
+
+
+let totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue);
